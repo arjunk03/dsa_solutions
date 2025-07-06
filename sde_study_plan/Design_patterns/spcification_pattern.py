da669e01f3bb6649d92d5344d41b17182521a14c
Arjun K <arjunk03@users.noreply.github.com>
2025-07-06 11:27:28 +0530

    updated solution

--- sde_study_plan/Design_patterns/spcification_pattern.py
+++ sde_study_plan/Design_patterns/spcification_pattern.py
@@ -37,9 +37,11 @@ class Specification:
         raise NotImplementedError("Must implement is_satisfied method")
 
     def __and__(self, other):
+        print("And called")
         return AndSpecification(self, other)
     
     def __or__(self, other):
+        print("Or called")
         return OrSpecification(self, other)
     
 
@@ -62,6 +64,7 @@ class OrSpecification(Specification):
         self.specifications = specifications
 
     def is_satisfied(self, item):
+        print("OrSpecification is satisfied called ", self.specifications)
         return any(spec.is_satisfied(item) for spec in self.specifications)
 
 class AndSpecification(Specification):
@@ -69,12 +72,14 @@ class AndSpecification(Specification):
         self.specifications = specifications
 
     def is_satisfied(self, item):
+        print("AndSpecification is satisfied called ", self.specifications)
         return all(spec.is_satisfied(item) for spec in self.specifications)
     
 
 class BetterFilter:
     @staticmethod
     def filter(products, specification):
+        print("BetterFilter is filtering products")
         return [p for p in products if specification.is_satisfied(p)]   
     
 
@@ -96,20 +101,22 @@ print("Filtered by size (MEDIUM):", [p.name for p in filtered_by_size])
 print("Filtered by color (GREEN) and size (LARGE):", [p.name for p in filtered_by_color_and_size])
 
 
-# Using Specification Pattern
-color_spec = ColorSpecification(Color.RED)
-size_spec = SizeSpecification(Size.MEDIUM)
-filtered_by_spec = BetterFilter.filter(products, color_spec & size_spec)
-print("Filtered by specification (RED and MEDIUM):", [p.name for p in filtered_by_spec])    
-# Using OrSpecification
-or_spec = color_spec | size_spec
-filtered_by_or_spec = BetterFilter.filter(products, or_spec)
-print("Filtered by OrSpecification (RED or MEDIUM):", [p.name for p in          filtered_by_or_spec])
-# Using AndSpecification
-and_spec = color_spec & size_spec
-filtered_by_and_spec = BetterFilter.filter(products, and_spec)
-print("Filtered by AndSpecification (RED and MEDIUM):", [p.name for p in filtered_by_and_spec])    
+# # Using Specification Pattern
+# color_spec = ColorSpecification(Color.RED)
+# size_spec = SizeSpecification(Size.MEDIUM)
+# print("Using Specification Pattern:", color_spec & size_spec)
+# filtered_by_spec = BetterFilter.filter(products, color_spec & size_spec)
+# print("Filtered by specification (RED and MEDIUM):", [p.name for p in filtered_by_spec])    
+# # Using OrSpecification
+# or_spec = color_spec | size_spec
+# filtered_by_or_spec = BetterFilter.filter(products, or_spec)
+# print("Filtered by OrSpecification (RED or MEDIUM):", [p.name for p in          filtered_by_or_spec])
+# # Using AndSpecification
+# and_spec = color_spec & size_spec
+# filtered_by_and_spec = BetterFilter.filter(products, and_spec)
+# print("Filtered by AndSpecification (RED and MEDIUM):", [p.name for p in filtered_by_and_spec])    
 # Using multiple specifications
-multi_spec = ColorSpecification(Color.RED) & SizeSpecification(Size.MEDIUM) | SizeSpecification(Size.LARGE)
+multi_spec = (ColorSpecification(Color.RED) & SizeSpecification(Size.MEDIUM)) | SizeSpecification(Size.LARGE)
+print("Using multiple specifications (RED and MEDIUM or LARGE):", multi_spec)
 filtered_by_multi_spec = BetterFilter.filter(products, multi_spec)
 print("Filtered by multiple specifications (RED and MEDIUM or LARGE):", [p.name for p in filtered_by_multi_spec])
